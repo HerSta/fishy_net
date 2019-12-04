0da1380e22319acf625a874331b39fcd5668b796
Herman Stavelin <hermanstavelin@gmail.com>
2019-12-04 18:55:53 +0100

    my own yolo. doesnt quite work

--- YOFO/darknet.py
+++ YOFO/darknet.py
@@ -4,8 +4,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+import cv2
 
 from utils import *
+
+
+def get_test_input():
+    img = cv2.imread("YOFO\\dog-cycle-car.png")
+    img = cv2.resize(img, (416, 416))
+    img_ = img[:,:,::-1].transpose((2,0,1))
+    img_ = img_[np.newaxis,:,:,:]/255.0
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+    return img_
 
 
 def parse_cfg(cfgfile):
@@ -76,7 +87,7 @@ def create_modules(blocks):
                 pad = 0
 
 
-            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias)
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias) #changed bias with dilation
             module.add_module("conv_{0}".format(index), conv)
 
             if batch_normalize:
@@ -89,7 +100,7 @@ def create_modules(blocks):
 
         elif (x["type"] == "upsample"):
             stride = int(x["stride"])
-            upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+            upsample = nn.Upsample(scale_factor = 2, mode = "bilinear", align_corners=True)
             module.add_module("upsample_{0}".format(index), upsample)
         
          #If it is a route layer
@@ -149,6 +160,7 @@ class Darknet(nn.Module):
         super(Darknet, self).__init__()
         self.blocks = parse_cfg(cfgfile)
         self.net_info, self.module_list = create_modules(self.blocks)
+        print("hi")
 
     def forward(self, x, CUDA):
         modules = self.blocks[1:]
@@ -166,14 +178,14 @@ class Darknet(nn.Module):
                 layers = [int(a) for a in layers]
 
                 if (layers[0] > 0):
-                    layers[0] = layers[0] - 1
+                    layers[0] = layers[0] - i
 
                 if len(layers) == 1:
                     x = outputs[i + layers[0]]
                 
                 else:
                     if (layers[1] > 0):
-                        layers[1] = layers[1] - 1
+                        layers[1] = layers[1] - i
                     
                     map1 = outputs[i + layers[0]]
                     map2 = outputs[i + layers[1]]
@@ -205,3 +217,92 @@ class Darknet(nn.Module):
         return detections
 
 
+    def  load_weights(self, weightfile):
+        fp = open(weightfile, "rb")
+
+        #The first 5 values are header information 
+        # 1. Major version number
+        # 2. Minor Version Number
+        # 3. Subversion number 
+        # 4,5. Images seen by the network (during training)
+        header = np.fromfile(fp, dtype = np.int32, count = 5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+
+
+        weights = np.fromfile(fp, dtype = np.float32)
+        ptr = 0
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i + 1]["type"]
+
+            #If module_type is convolutional load weights
+            #Otherwise ignore.
+
+            if module_type == "convolutional":
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i+1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+
+                conv = model[0]
+
+            if (batch_normalize):
+                bn = model[1]
+
+                #Get the number of weights of Batch Norm Layer
+                num_bn_biases = bn.bias.numel()
+
+                #Load the weights
+                bn_biases = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                ptr += num_bn_biases
+
+                bn_weights = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                ptr  += num_bn_biases
+
+                bn_running_mean = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                ptr  += num_bn_biases
+
+                bn_running_var = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                ptr  += num_bn_biases
+
+                #Cast the loaded weights into dims of model weights. 
+                bn_biases = bn_biases.view_as(bn.bias.data)
+                bn_weights = bn_weights.view_as(bn.weight.data)
+                bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                #Copy the data to model
+                bn.bias.data.copy_(bn_biases)
+                bn.weight.data.copy_(bn_weights)
+                bn.running_mean.copy_(bn_running_mean)
+                bn.running_var.copy_(bn_running_var)
+            else:
+                #Number of biases
+                num_biases = conv.bias.numel()
+
+                #Load the weights
+                conv_biases = torch.from_numpy(weights[ptr: ptr + num_biases])
+                ptr = ptr + num_biases
+
+                #reshape the loaded weights according to the dims of the model weights
+                conv_biases = conv_biases.view_as(conv.bias.data)
+
+                #Finally copy the data
+                conv.bias.data.copy_(conv_biases)
+
+        #Let us load the weights for the Convolutional layers
+        num_weights = conv.weight.numel()
+
+        #Do the same as above for weights
+        conv_weights = torch.from_numpy(weights[ptr:ptr+num_weights])
+        ptr = ptr + num_weights
+
+        conv_weights = conv_weights.view_as(conv.weight.data)
+        conv.weight.data.copy_(conv_weights)
+
+model = Darknet("YOFO\\yofov3.cfg")
+model.load_weights("YOFO\\yolov3.weights")
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print(pred)
