ed00e9daf40d52cbd2cbf7fd499a5f6981156f7c
Herman Stavelin <hermanstavelin@gmail.com>
2019-12-12 13:12:58 +0100

    added ability to extract weights an intermediate images

--- YOFO/detect.py
+++ YOFO/detect.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 import cv2 
-from utils import *
+from util import *
 import argparse
 import os 
 import os.path as osp
@@ -13,7 +13,6 @@ from darknet import Darknet
 import pickle as pkl
 import pandas as pd
 import random
-
 
 def arg_parse():
     """
@@ -30,14 +29,14 @@ def arg_parse():
                         "Image / Directory to store detections to",
                         default = "det", type = str)
     parser.add_argument("--bs", dest = "bs", help = "Batch size", default = 1)
-    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
+    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.25)
     parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
     parser.add_argument("--cfg", dest = 'cfgfile', help = 
                         "Config file",
-                        default = "YOFO/yofov3.cfg", type = str)
+                        default = "cfg/yolov3.cfg", type = str)
     parser.add_argument("--weights", dest = 'weightsfile', help = 
                         "weightsfile",
-                        default = "YOFO/yolov3.weights", type = str)
+                        default = "fish2_last.weights", type = str)
     parser.add_argument("--reso", dest = 'reso', help = 
                         "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
                         default = "416", type = str)
@@ -50,16 +49,13 @@ batch_size = int(args.bs)
 confidence = float(args.confidence)
 nms_thesh = float(args.nms_thresh)
 start = 0
-CUDA = torch.cuda.is_available()
-
-def load_classes(namesfile):
-    fp = open(namesfile, "r")
-    names = fp.read().split("\n")[:-1]
-    return names
+CUDA = False
 
 
-num_classes = 80    #For COCO
-classes = load_classes("YOFO/data/coco.names")
+
+num_classes = 1
+classes = load_classes("data/fish.names")
+
 
 
 #Set up the neural network
@@ -73,13 +69,14 @@ inp_dim = int(model.net_info["height"])
 assert inp_dim % 32 == 0 
 assert inp_dim > 32
 
-#If there's a GPU availible, put the model on GPU
-if CUDA:
-    model.cuda()
+
+modules = model.module_list
+
+
+
 
 #Set the model in evaluation mode
 model.eval()
-
 
 read_dir = time.time()
 #Detection phase
@@ -91,111 +88,161 @@ except NotADirectoryError:
 except FileNotFoundError:
     print ("No file or directory with the name {}".format(images))
     exit()
-
-
+    
 if not os.path.exists(args.det):
     os.makedirs(args.det)
 
 load_batch = time.time()
 loaded_ims = [cv2.imread(x) for x in imlist]
 
-
-#PyTorch Variables for images
 im_batches = list(map(prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
-
-#List containing dimensions of original images
 im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
 im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
-
-if CUDA:
-    im_dim_list = im_dim_list.cuda()
 
 
 leftover = 0
 if (len(im_dim_list) % batch_size):
-   leftover = 1
+    leftover = 1
 
 if batch_size != 1:
-   num_batches = len(imlist) // batch_size + leftover            
-   im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
-                       len(im_batches))]))  for i in range(num_batches)]  
-
-
+    num_batches = len(imlist) // batch_size + leftover            
+    im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
+                        len(im_batches))]))  for i in range(num_batches)]  
 
 write = 0
+
+
+
+import matplotlib.pyplot as plt
+    
 start_det_loop = time.time()
 for i, batch in enumerate(im_batches):
-    #load the image 
+#load the image 
     start = time.time()
-    if CUDA:
-        batch = batch.cuda()
 
-    prediction = model(Variable(batch, volatile = True), CUDA)
+    with torch.no_grad():
+        prediction = model(Variable(batch), False)
 
-    prediction = write_results(prediction, confidence, num_classes, nms_conf = nms_thesh)
+    print(prediction)
 
-    end = time.time()
 
-    if type(prediction) == int:
+    for pred in prediction:
+        for idx,image in enumerate(pred):
+            if idx == 8:
+                break
+            plt.imshow(image)
+            plt.axis('off')
+            plt.savefig("C:\\Users\\peterhs\\Dropbox\\Apper\\Overleaf\\Project_thesis\\figures\\internal_yolo\\layer_last_" + str(idx), bbox_inches="tight")
+            
+            #plt.show()
 
-        for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
-            im_id = i*batch_size + im_num
-            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
-            print("{0:20s} {1:s}".format("Objects Detected:", ""))
-            print("----------------------------------------------------------")
-        continue
+#   prediction = write_results(prediction, confidence, num_classes, nms_conf = nms_thesh)
+#
+#    end = time.time()
+#
+#    if type(prediction) == int:
+#
+#        for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+#            im_id = i*batch_size + im_num
+#            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+#            print("{0:20s} {1:s}".format("Objects Detected:", ""))
+#            print("----------------------------------------------------------")
+#        continue
+#
+#    prediction[:,0] += i*batch_size    #transform the attribute from index in batch to index in imlist 
+#
+#    if not write:                      #If we have't initialised output
+#        output = prediction  
+#        write = 1
+#    else:
+#        output = torch.cat((output,prediction))
+#
+#    for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+#        im_id = i*batch_size + im_num
+#        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+#        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+#        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
+#        print("----------------------------------------------------------")
+    
 
-    prediction[:,0] += i*batch_size    #transform the atribute from index in batch to index in imlist 
 
-    if not write:                      #If we have't initialised output
-        output = prediction  
-        write = 1
-    else:
-        output = torch.cat((output,prediction))
+def first_convolution():
+    #plotting our actual image
+    my_img = prep_image_plottable(loaded_ims[0], inp_dim)
+    #plt.imshow(my_img)
 
-    for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
-        im_id = i*batch_size + im_num
-        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
-        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
-        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
-        print("----------------------------------------------------------")
+    #fetch the weights from the first layer
+    weights = modules[0].conv_0.weight
 
-    if CUDA:
-        torch.cuda.synchronize()       
 
+    color_channels = 3
+    from scipy import ndimage, signal
+    for i in range(32):
+        filter = weights.data.numpy()[i] #3x3x3 matrix
+
+        #filter = np.array([[[0,0,0],  [0,0,0], [0,0,0]], [[0,0,0], [0,1,0],  [0,0,0]], [[0,0,0],  [0,0,0], [0,0,0]]])
+        pixel_grid = np.zeros((416,416))
+        for j in range(color_channels):
+            pixel_grid += ndimage.convolve(my_img[:,:,j], filter[:,:,j], mode="constant", cval=0.0)
+            #print(j)
+
+        #filtered_img = ndimage.convolve(my_img, filter)
+        pixel_grid_cropped = pixel_grid[91:325,:]
+        plt.imshow(pixel_grid_cropped, cmap="gray")
+        plt.axis('off')
+        #plt.savefig("C:\\Users\\peterhs\\Dropbox\\Apper\\Overleaf\\Project_thesis\\figures\\internal_yolo\\layer_last_" + str(i), bbox_inches="tight")
+        #plt.show()
+        
+
+    #apply the weights to the image
+
+
+    #dont forget about bias and activation function!!!!
+
+    #plot image
+    #plt.show()
+
+    print("hi")
+
+
+first_convolution()
+   
 try:
     output
 except NameError:
     print ("No detections were made")
     exit()
 
-
 im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
 
-scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)
+scaling_factor = torch.min(416/im_dim_list,1)[0].view(-1,1)
 
 
 output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
 output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
+
+
 
 output[:,1:5] /= scaling_factor
 
 for i in range(output.shape[0]):
     output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim_list[i,0])
     output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim_list[i,1])
-
-
+    
+    
+output_recast = time.time()
 class_load = time.time()
-colors = pkl.load(open("YOFO/pallete", "rb"))
-
+colors = pkl.load(open("pallete", "rb"))
 
 draw = time.time()
 
-def write(x, results, color):
+
+def write(x, results):
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
     img = results[int(x[0])]
     cls = int(x[-1])
+    color = random.choice(colors)
     label = "{0}".format(classes[cls])
     cv2.rectangle(img, c1, c2,color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
@@ -205,26 +252,24 @@ def write(x, results, color):
     return img
 
 
-list(map(lambda x: write(x, loaded_ims, colors[0]), output))
-det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
+list(map(lambda x: write(x, loaded_ims), output))
+
+det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("\\")[-1]))
+
 list(map(cv2.imwrite, det_names, loaded_ims))
+
+
 end = time.time()
 
-
-
-############ TIMING SHIT
 print("SUMMARY")
 print("----------------------------------------------------------")
 print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
 print()
 print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
 print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
-#print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) +  " images)", output_recast - start_det_loop))
-#print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
+print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) +  " images)", output_recast - start_det_loop))
+print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
 print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
 print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch)/len(imlist)))
 print("----------------------------------------------------------")
 
-
-
-torch.cuda.empty_cache()
